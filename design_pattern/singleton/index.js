1f6f177851c37eee8a7195192a88a45f32cfcad9
wovert <67668283@qq.com>
2019-03-13 16:03:55 +0800

    singleton design pattern

---
+++ design_pattern/singleton/index.js
@@ -0,0 +1,19 @@
+var name = 'wovert'
+var age = 18
+var gender = 'girl'
+
+var name = 'lingyima'
+var age = 81
+var gender = 'boy'
+
+var person1 = {
+  name: 'wovert',
+  age: 18
+}
+
+var person2 = {
+  name: 'lingyima',
+  age: 81
+}
+
+
